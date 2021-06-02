4ff371d110f6f01e1e48cb4de267632765207d89
Merida-Daniel <dacastamerd@gmail.com>
2021-06-01 22:50:50 -0500

    Se inicio el FRONT del CRUD, se agrego el componente List y UseEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useReducer, useEffect} from 'react';
+
+const HOST_API = "http://localhots:8080/api"
+const initialState = {
+  list: []
+}
+const Store = createContext(initialState)
+
+const List = () => {
+  const { dispatch, state } = useContext(Store);
+
+  useEffect(() => {
+    fetch(HOST_API + "/todos")
+      .then(response => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list })
+      })
+  }, [state.list.length, dispatch]);
+  
+  return <div>
+  <table>
+    <thead>
+      <tr>
+        <td>ID</td>
+        <td>Nombre</td>
+        <td>¿Esta completado?</td>
+      </tr>
+    </thead>
+    <tbody>
+      {state.list.map((todo) => {
+        return <tr key={todo.id}>
+          <td>{todo.id}</td>
+          <td>{todo.name}</td>
+          <td>{todo.isCompleted }</td>      
+          </tr>
+      })}
+    </tbody>
+  </table>
+</div>
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'update-list':
+      return { ...state, list: action.list }
+    case 'add-item':
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList }
+    default:
+      return state;
+
+  }
+}
+
+const StoreProvider = ({ children }) => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <Store.Provider value={{ state, dispatch }}>
+    {children}
+  </Store.Provider>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <StoreProvider>
+    <List />
+  </StoreProvider>
 }
 
 export default App;
